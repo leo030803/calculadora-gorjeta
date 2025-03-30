4142689639a693c896977d0e708c587f574ed608
bragaleonardo946 <bragaleonardo946@gmail.com>
2025-03-30 19:37:24 +0000

    Função para calcular a gorjeta e o total por pessoa finalizada

--- main.js
+++ main.js
@@ -5,18 +5,20 @@ let buttonSelected = null
 
 function receiveBillValue() {
     bill = document.querySelector("#bill").valueAsNumber
+    calculate()
 
     
 }
 
 function receiveNumberOfPeopleValue() {
      numberOfPeople = document.querySelector("#people").valueAsNumber
-     
+     calculate()
+
 
 }
 
 function receiveTipPercentageValue(value) {
-    if(buttonSelected !== null) {
+    if (buttonSelected !== null) {
        buttonSelected.classList.remove("button-selected")
 
     }
@@ -24,7 +26,7 @@ function receiveTipPercentageValue(value) {
     let customTipInput = document.querySelector("#custom-tip")
 
     
-   if(customTipInput.value !== "") {
+   if (customTipInput.value !== "") {
         customTipInput.value = ""
 
     }
@@ -34,19 +36,39 @@ function receiveTipPercentageValue(value) {
 
     buttonSelected = document.querySelector(`#button-${value}`)
     buttonSelected.classList.add("button-selected")
+    calculate()
 
 
 }
 
 function receiveCustomTipPercentageValue() {
-      if(buttonSelected !== null) {
+      if (buttonSelected !== null) {
        buttonSelected.classList.remove("button-selected")
        buttonSelected = null
 
       }
 
       tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+      calculate()
       
 
+
+}
+
+function calculate() {
+      if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+           let amountStrong = document.querySelector(".amount strong")
+           let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+
+           amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
+
+           let totalStrong = document.querySelector(".total strong")
+           let totalPerson = (bill / numberOfPeople) + tipAmountPerson
+           totalStrong.innerText = `$${totalPerson.toFixed(2)}`
+
+      }  else {
+           console.log("preencha tudo")
+
+      }
 
 }
